4289f93c4790e87f6b4742939b04ae3bb5a30360
BrayanCahuanaU <brayan.cahuana@ucsm.edu.pe>
2026-03-19 00:53:52 -0500

    V1.0

--- BackendERP/main.py
+++ BackendERP/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from db import get_connection
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -6,38 +6,73 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # en producción restringir
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/")
-def root():
-    try:
-        conn = get_connection()
-        cur = conn.cursor()
-
-        cur.execute("SELECT COUNT(*) FROM a01mest;")
-        result = cur.fetchone()
-
-        cur.close()
-        conn.close()
-
-        return {"registros": result[0]}
-
-    except Exception as e:
-        return {"error": str(e)}
-    
-@app.get("/clientes")
-def get_clientes():
+@app.post("/")
+async def main(request: Request):
+    body = await request.json()
     conn = get_connection()
     cur = conn.cursor()
 
-    cur.execute("SELECT * FROM a01mest LIMIT 10;")
-    rows = cur.fetchall()
+    try:
+        if body["ID"] == "DHB0016":
+            # LISTA
+            cur.execute("""
+                SELECT ccodest AS id, cuniaca AS cequipo
+                FROM a01mest
+                LIMIT 20;
+            """)
+            rows = cur.fetchall()
 
-    cur.close()
-    conn.close()
+            data = []
+            for r in rows:
+                data.append({
+                    "ID": r[0],
+                    "CEQUIPO": r[1]
+                })
 
-    return {"data": rows}
+            return data
+
+        elif body["ID"] == "DHB0017":
+            # DETALLE
+            cur.execute("""
+                SELECT 
+                    ccodest AS id,
+                    cuniaca AS clabora,
+                    degreso AS dfecha,
+                    cplaest AS ctipmto,
+                    cnrodni AS ctecnic,
+                    cestado AS cestado
+                    FROM a01mest
+                LIMIT 10;
+            """)
+            rows = cur.fetchall()
+
+            return {
+                "ID": body["CEQUIPO"],
+                "CEQUIPO": body["CEQUIPO"],
+                "DATOS": [
+                    {
+                        "ID": r[0],
+                        "CLABORA": r[1],
+                        "DFECHA": str(r[2]),
+                        "CTIPMTO": r[3],
+                        "CTECNIC": r[4],
+                        "CESTADO": r[5]
+                    } for r in rows
+                ]
+            }
+
+        else:
+            return {"ERROR": "ID no reconocido"}
+
+    except Exception as e:
+        return {"ERROR": str(e)}
+
+    finally:
+        cur.close()
+        conn.close()
